5e6ba71fee304e101ce3e36f13e1749045a8dea9
Alice Chang <sinsofangels@gmail.com>
2020-11-24 00:05:23 -0500

    Set up main app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+
+const foods = [
+  "Pierogis",
+  "Spaghetti",
+  "Goulash",
+  "Enchilada",
+  "Chimichanga",
+  "Dumplings",
+  "Pancakes",
+  "Waffles",
+  "Bananas",
+  "Bagels",
+  "Tacos",
+  "Cheetos",
+  "Salsa",
+  "Lasagna",
+  "Ravioli",
+  "Tater Tots",
+  "Breadsticks",
+  "Falafel",
+  "Clams",
+  "Petit Fours",
+  "Egg rolls",
+  "Tostadas",
+  "Schwarma",
+  "Empanadas",
+  "Gnocchi",
+  "Samosas",
+  "Fluffernutter",
+  "Hamburgers",
+  "Vegemite",
+  "Muffuletta",
+  "Pastrami",
+  "Souvlaki"
+]
 
 function App() {
+  const [name, setName] = useState("");
+  const [mobName, setMobName] = useState("");
+
+  function changeHandler(e) {
+    setName(e.target.value);
+  }
+
+  function submit(e) {
+    e.preventDefault();
+    setName("");
+    function pickName() {
+      const randomNumber = Math.floor(Math.random()*33);
+      return `${name} ${foods[randomNumber]}`;
+    }
+    setMobName(pickName());
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submit}>
+        <label htmlFor="name">Name: </label>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={changeHandler}
+          />
+        {mobName && 
+          <div className="results">
+            <p className="name">Congratulations! Your mob name is {mobName}!</p>
+          </div>
+        }
+      </form>
     </div>
   );
 }
